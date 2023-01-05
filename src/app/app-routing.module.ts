06387b4be15d1ecca1de14125dc88f7013445923
Anil Kumar <53567331+Anilkumar0143@users.noreply.github.com>
2023-01-05 18:23:13 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,36 +58,6 @@ const routes: Routes = [
   { path: 'add-project', component:ProjectFormComponent  },
   { path: 'add-user', component:UserFormComponent  },
   
-
-  { path: 'alerts', component: AlertsComponent },
-  { path: 'accordion', component: AccordionComponent },
-  { path: 'badges', component: BadgesComponent },
-  { path: 'breadcrumbs', component: BreadcrumbsComponent },
-  { path: 'buttons', component: ButtonsComponent },
-  { path: 'cards', component: CardsComponent },
-  { path: 'carousel', component: CarouselComponent },
-  { path: 'charts-apexcharts', component: ChartsApexchartsComponent },
-  { path: 'charts-chartjs', component: ChartsChartjsComponent },
-  { path: 'form-editors', component: FormsEditorsComponent },
-  { path: 'form-elements', component: FormsElementsComponent },
-  { path: 'form-layouts', component: FormsLayoutsComponent },
-  { path: 'icons-bootstrap', component: IconsBootstrapComponent },
-  { path: 'icons-boxicons', component: IconsBoxiconsComponent },
-  { path: 'icons-remix', component: IconsRemixComponent },
-  { path: 'list-group', component: ListGroupComponent },
-  { path: 'modal', component: ModalComponent },
-  { path: 'pagination', component: PaginationComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'spinners', component: SpinnersComponent },
-  { path: 'tables-data', component: TablesDataComponent },
-  { path: 'tables-general', component: TablesGeneralComponent },
-  { path: 'tabs', component: TabsComponent },
-  { path: 'tooltips', component: TooltipsComponent },
-  { path: 'pages-blank', component: PagesBlankComponent },
-  { path: 'pages-contact', component: PagesContactComponent },
-  { path: 'pages-error404', component: PagesError404Component },
-  { path: 'pages-faq', component: PagesFaqComponent },
-
 ];
 
 @NgModule({
