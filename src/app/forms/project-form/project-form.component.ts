7c7833f7b338b339a30db6617c58e8f04aeba479
Anil Kumar <53567331+Anilkumar0143@users.noreply.github.com>
2023-01-05 18:26:15 +0530

    Update project-form.component.ts

--- src/app/forms/project-form/project-form.component.ts
+++ src/app/forms/project-form/project-form.component.ts
@@ -8,7 +8,8 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class ProjectFormComponent implements OnInit {
   toggleaddperson: boolean = true;
-  url = 'http://localhost:3000/project';
+ // url = 'http://localhost:3000/project';
+    url = 'https://projectmanagementdb1.vercel.app//project';
 
   projectForm!: FormGroup;
 
